32ba8e1113d02c3e7d3ef3317d89d3fa4293b3ec
Geraldo157 <gsmecanica76@gmail.com>
2023-05-21 14:47:29 -0300

    Adicionando about page e linkando a nav bar

--- task/views.py
+++ task/views.py
@@ -6,3 +6,7 @@ from django.views.decorators.http import require_http_methods
 @require_http_methods(["GET", "POST"])
 def home(request):
     return render(request, "home.html")
+
+
+def about(request):
+    return render(request, "about.html")
