07f7ce31fc970188d16c8b05d9c86176e849bcdc
kenbme <guilhermekenned1@gmail.com>
2023-05-21 12:40:34 -0300

    setup templates

--- task/models.py
+++ task/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Task(models.Model):
+    owner = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    category = models.CharField(max_length=50)
+    description = models.CharField(max_length=250)
+    date = models.DateField()
+    ends_in = models.CharField(max_length=5)  # 17:23 H:M
